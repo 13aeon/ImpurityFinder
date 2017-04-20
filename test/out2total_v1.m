85d2aee9c3894f08cd4d45e93778f4c90afd7005
PrinceS17 <sjh14@mails.tsinghua.edu.cn>
2017-04-20 01:12:57 -0500

    Add the part of writing result from svm model.

---
+++ test/out2total_v1.m
@@ -0,0 +1,13 @@
+clear
+load out2total.mat
+
+[test_out,score]= predict(SVMModel,test_data);
+% plotroc(label_test',score(:,2)');
+[rpr,fpr,thresholds]= roc(label_test',score(:,2)');
+[~,~,~,AUC]= perfcurve(label_test',score(:,2)',1);
+idr= find(rpr> 0.9,1,'first');
+output= score(:,2)> thresholds(idr);
+write_result_v1(test_pic,index_t,output);
+AUC,
+
+% out to total: AUC only 0.8078
---
+++ test/write_result_v1.m
@@ -0,0 +1,16 @@
+function y= write_result_v1(test_pic,index,output)
+% print classification result to result.txt; write in circle structure
+
+file= fopen('predict.txt','w');
+sz= size(index);
+num= sz(2);
+for i=1:num
+    name= test_pic(index(i)).name;
+    str=num2str(output(i));
+    fprintf(file,'%s %s\n',name,str);
+end
+
+y= 1;
+end
+ 
+
